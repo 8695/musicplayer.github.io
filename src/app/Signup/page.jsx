10b3f4ac212e6fafc94cc6fe01eca0b334b586b8
NIRANJAN MOURYA <NIRANJANMOURYA000@GMAIL.COM>
2024-09-26 22:39:38 +0530

    upload

--- src/app/Signup/page.jsx
+++ src/app/Signup/page.jsx
@@ -16,8 +16,7 @@ import { useRouter } from 'next/navigation';
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 
-export const description =
-  "A sign up form with first name, last name, email and password inside a card. There's an option to sign up with GitHub and a link to login if you already have an account";
+
 
 export default function SignUp() {
   const router = useRouter();
